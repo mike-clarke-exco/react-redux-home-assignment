3511d06a9070a6145085f3dc159214e4e312c0e7
Yaniv <yaniv@playbuzz.com>
2022-03-14 09:47:23 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import SortableComponent from "./components/App";
 
+ReactDOM.render(
+  <React.StrictMode>
+    <SortableComponent />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
