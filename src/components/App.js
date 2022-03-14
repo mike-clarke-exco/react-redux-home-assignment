c2edb18d0a687a760ed5c0cce9a815f5034d6b18
Yaniv <yaniv@playbuzz.com>
2022-03-14 09:48:38 +0200

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import {
+  arrayMove,
+  SortableContainer,
+  SortableElement,
+} from "react-sortable-hoc";
+import { ITEMS } from "./data";
+
+import "./App.css";
+
+const SortableItem = SortableElement(({ value }) => (
+  <tr>
+    <td>{value.totalHours}</td>
+    <td>{value.Hours}</td>
+    <td>{value.manualHours}</td>
+    <td>{value.ExtraHours}</td>
+    <td className="td-worker-name">{value.workerName}</td>
+    <td className="td-id">{value.id}</td>
+  </tr>
+
+  // <li tabIndex={0}>{value}</li>
+));
+
+const SortableList = SortableContainer(({ items }) => {
+  return (
+    <table>
+      <th>סך כל שעות</th>
+      <th>שעות</th>
+      <th>שעות ידניות</th>
+      <th>שעות חריגות</th>
+      <th>שם העובד</th>
+      <th>מספר ת.ז</th>
+      {items.map((value, index) => (
+        <SortableItem
+          key={`item-${value.workerName}`}
+          index={index}
+          value={value}
+        />
+      ))}
+    </table>
+  );
+});
+
+function SortableComponent() {
+  const [data, setData] = useState(ITEMS);
+
+  let onSortEnd = ({ oldIndex, newIndex }) => {
+    setData(() => arrayMove(data, oldIndex, newIndex));
+  };
+
+  return (
+    <div className="table-container">
+      <SortableList items={data} onSortEnd={onSortEnd} />
+    </div>
+  );
+}
+export default SortableComponent;
