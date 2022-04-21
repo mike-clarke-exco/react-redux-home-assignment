f147f99164355f2d4b90aa74873c574044b1ff2b
Shajil Thottungal <shajil@live.com>
2022-04-21 18:24:40 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SortableComponent from './components/App';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from '../src/redux/store';
 import './index.css';
 
 ReactDOM.render(
-    <SortableComponent />,
-    document.getElementById("root")
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
