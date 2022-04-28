dd0934f1620436db826583d7a9f49b0fbd81e98f
Arndt Knoetze <arndt@silvertree.holdings>
2022-04-28 13:32:57 +0200

    error

--- src/index.js
+++ src/index.js
@@ -12,10 +12,8 @@ const store = createStore(() => ({
 }));
 
 ReactDOM.render(
-    <React.StrictMode>
         <Provider store={store}>
             <App />
-        </Provider>
-    </React.StrictMode >,
+        </Provider>,
     document.getElementById("root")
 );
